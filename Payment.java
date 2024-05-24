4e6f5787171f8834fabcf703c0958b2b7acbb2b7
NikosAndromidas <84805174+NikosAndromidas@users.noreply.github.com>
2024-05-24 09:45:09 +0300

    Add files via upload

---
+++ Payment.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;     
+import javax.swing.JLabel;     
+import javax.swing.JButton;    
+import javax.swing.JOptionPane; 
+import java.awt.FlowLayout;  
+
+
+public class Payment extends JFrame {
+    public Payment(long days, long totalCost) {
+        setTitle("Πληρωμή");
+        setSize(300, 200);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        add(new JLabel("Συνολικό Κόστος: " + totalCost + "€"));
+        JButton payButton = new JButton("Πληρωμή με αντικαταβολή");
+        add(payButton);
+        payButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(this, "Η πληρωμή σας θα γίνει την ώρα του check-in! Ευχαριστούμε για την προτίμηση σας!");
+            dispose();
+        });
+
+        setVisible(true);
+    }
+}
+
